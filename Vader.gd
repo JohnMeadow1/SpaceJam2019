624420c6f97765488cd753c40b6a111a7771e92e
John_Meadow <armitage3@gazeta.pl>
2019-12-07 08:29:58 +0100

    separated vader from vader

---
+++ Vader.gd
@@ -0,0 +1,76 @@
+extends KinematicBody2D
+
+const MAX_DISTANCE := 400.0
+const MAX_DISTANCE_SQ := MAX_DISTANCE * MAX_DISTANCE
+
+onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+onready var audio2: AudioStreamPlayer2D = $AudioStreamPlayer2D2
+
+var darkning: Node2D
+var target: Node2D
+
+func _process(delta: float) -> void:
+	$Light2D.rotation = (get_global_mouse_position() - $Light2D.global_position).angle()
+	
+	var move: Vector2
+	move.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	move.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	if move.length() > 0:
+		var direction = int(round(((move.angle() + PI) / PI) * 2))
+		match direction:
+			1:$AnimatedSprite.play("up")
+			2:$AnimatedSprite.play("right")
+			3:$AnimatedSprite.play("down")
+			4:$AnimatedSprite.play("left")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	
+	move_and_slide(move.normalized() * 200)
+	
+	if darkning:
+		darkning.from = $DarkningSource.global_position
+		target.darken(delta)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.is_action("unlimited_power") and not darkning:
+			var lantern: Node2D
+			var min_dist := MAX_DISTANCE_SQ
+			
+			for l in get_tree().get_nodes_in_group("light"):
+				var dist := global_position.distance_squared_to(l.global_position)
+				if dist < min_dist:
+					lantern = l
+					min_dist = dist
+			
+			if lantern:
+				target = lantern
+				darkning = preload("res://Darkning.tscn").instance()
+				darkning.from = $DarkningSource.global_position
+				darkning.to = lantern.global_position
+				get_parent().add_child(darkning)
+				audio.play()
+				if randi() % 3 == 0:
+					audio2.play()
+		
+		if not event.pressed and event.is_action("unlimited_power") and darkning:
+			darkning.queue_free()
+			target.reset()
+			darkning = null
+			target = null
+			audio.stop()
+		
+		if event.pressed and event.is_action("click"):
+			play3()
+
+func play3():
+	if !$AudioStreamPlayer2D3.playing:
+		if randi() % 3 != 0:
+			return
+		
+		$AudioStreamPlayer2D3.stream = [
+			preload("res://sounds/somethingComplete.wav"),
+			preload("res://sounds/somethingDarkSide.wav")
+		][randi() % 2]
+		$AudioStreamPlayer2D3.play()

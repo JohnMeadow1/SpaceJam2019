2d81de63bb101750e42e956429576f7f1191163c
Tomasz Chabora <kobewi4e@gmail.com>
2019-12-07 01:18:54 +0100

    Piorunowanie latarniów

--- Lantern.gd
+++ Lantern.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 const CHARGE_MAX := 1.0
 
@@ -10,21 +10,20 @@ var charge_timer: float
 
 onready var size_in_pixels:float = $Sprite.texture.get_size().x * scale.x
 
-func _process(delta: float) -> void:
+func darken(delta: float) -> void:
+	charge_timer += delta
 	
-	if dark_side_in:
-		charge_timer += delta
-		
-		var c := 1 - charge_timer / CHARGE_MAX
-		sprite.modulate = Color(c, c, c)
-		
-		if charge_timer >= CHARGE_MAX:
-			dark.show()
+	var c := 1 - charge_timer / CHARGE_MAX
+	sprite.modulate = Color(c, c, c)
+	
+	if charge_timer >= CHARGE_MAX:
+		dark.show()
 #		size_in_pixels = $Sprite.texture.get_size().x * scale.x
-	else:
-		charge_timer = 0
-		if not dark.visible:
-			sprite.modulate = Color.white
+
+func reset():
+	charge_timer = 0
+	if not dark.visible:
+		sprite.modulate = Color.white
 
 func something_entered(body: Node) -> void:
 	if body.is_in_group("darkside"):

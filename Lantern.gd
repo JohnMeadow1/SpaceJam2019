bd2ddada6ad2e4896e9c652b04047e891878ff89
John_Meadow <armitage3@gazeta.pl>
2019-12-07 01:20:37 +0100

    added run away

--- Lantern.gd
+++ Lantern.gd
@@ -6,9 +6,10 @@ onready var sprite: Sprite = $Sprite
 onready var dark: Node2D = $LightScale
 
 var dark_side_in: bool
+var darkness_is_on: bool = false
 var charge_timer: float
 
-onready var size_in_pixels:float = $Sprite.texture.get_size().x * scale.x
+onready var size_in_pixels:float = $LightScale/Light2D.texture.get_size().x * scale.x
 
 func _process(delta: float) -> void:
 	
@@ -20,10 +21,11 @@ func _process(delta: float) -> void:
 		
 		if charge_timer >= CHARGE_MAX:
 			dark.show()
-#		size_in_pixels = $Sprite.texture.get_size().x * scale.x
+			darkness_is_on = true
+
 	else:
 		charge_timer = 0
-		if not dark.visible:
+		if darkness_is_on:
 			sprite.modulate = Color.white
 
 func something_entered(body: Node) -> void:

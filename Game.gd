354b5b77ee735b5e79e1089990cd524b6cb87ecc
Tomasz Chabora <kobewi4e@gmail.com>
2019-12-07 15:16:22 +0100

    Full counter!

--- Game.gd
+++ Game.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 var darkside_count: int setget set_count
+onready var full_counter: int = $YSort/Jakes.get_child_count()
+
+func _ready() -> void:
+	set_count(0)
 
 func set_count(c: int):
 	darkside_count = c
-	$UI/Score/Count.text = str(c)
+	$UI/Score/Count.text = str(c, "/", full_counter)

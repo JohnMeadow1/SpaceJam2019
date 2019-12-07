8958180739ba9f33de93657ffc0e5164e88d9286
Tomasz Chabora <kobewi4e@gmail.com>
2019-12-07 12:54:10 +0100

    Dżewa

---
+++ decorations/FadeObject.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var player_in: bool
+
+func _process(delta: float) -> void:
+	if player_in:
+		if modulate.a > 0.3:
+			modulate.a -= delta
+	else:
+		if modulate.a < 1:
+			modulate.a += delta
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body.is_in_group("darkside"):
+		player_in = true
+
+func _on_Area2D_body_exited(body: Node) -> void:
+	if body.is_in_group("darkside"):
+		player_in = false
